8184150c825cf9a552760631d66a42316ebb90d1
huangchengdu <huang2009303513@gmail.com>
2019-03-14 16:39:44 +0800

    update

--- reactNativePullRefreshExample/ios/reactNativePullRefreshExampleTests/reactNativePullRefreshExampleTests.m
+++ reactNativePullRefreshExample/ios/reactNativePullRefreshExampleTests/reactNativePullRefreshExampleTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface reactNativePullRefreshExampleTests : XCTestCase
+@interface reactnativepullrefreshexampleTests : XCTestCase
 
 @end
 
-@implementation reactNativePullRefreshExampleTests
+@implementation reactnativepullrefreshexampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
